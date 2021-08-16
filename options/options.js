1bd636dfbc6340bef0d48a65eebdf8ba13b4d23e
Crowdbotics <team@crowdbotics.com>
2021-08-16 19:51:01 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "purple_mountain_29856Identifier",
+  url: "https://purple_mountain_29856Identifier.botics.co",
+  api: "https://purple_mountain_29856Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
